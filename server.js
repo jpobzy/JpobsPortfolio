4cc3cf5f1925b5054b3743c3bb5fa1a7f1756fd8
Joey <77253943+jpobzy@users.noreply.github.com>
2023-01-11 20:01:48 -0500

    can now scroll down to header sections when clicked on

--- server.js
+++ server.js
@@ -16,35 +16,12 @@ app.set('views', path.join(__dirname, 'views'));
 
 // use res.render to load up an ejs view file
 
+app.use(express.static(__dirname + '/public'));
+
 // index page
 app.get('/', function(req, res) {
-  var mascots = [
-    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-    { name: 'Tux', organization: "Linux", birth_year: 1996},
-    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-  ];
-  var tagline = "No programming concept is complete without a cute animal mascot.";
-
-  res.render('pages/index', {
-    mascots: mascots,
-    tagline: tagline
-  });
-});
-
-// about page
-app.use(express.static(__dirname + '/public'));
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
-
-// projects page
-app.get('/projects', function(req, res) {
-  res.render('pages/projects');
-});
-
-//contact info page
-app.get('/contact-info', function(req, res) {
-  res.render('pages/contact-info');
+  // res.render('pages/test');
+  res.render('pages/index');
 });
 
 
