0c15531d2c6fb2cd1f68eed1d44df30d0d60d888
Joey <77253943+jpobzy@users.noreply.github.com>
2023-01-13 16:59:16 -0500

    fixed things

--- actions.js
+++ actions.js
@@ -1,7 +1,60 @@
-const button = document.getElementById('scroll-to-top-button');
+const button = document.getElementById('scroll-to-top-btn');
 button.addEventListener('click', event => {
   window.scrollTo({
     top: 0,
     behavior: 'smooth'
   });
+});
+
+var footer = document.getElementById("footer");
+var footerHeight = footer.clientHeight;
+
+scrollToTopBtn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0 - footerHeight,
+    behavior: "smooth"
+  });
+});
+
+
+
+
+
+
+
+
+
+
+function scrollToAboutMe() {
+  var aboutMe = document.querySelector(".aboutme-title");
+  var aboutMePosition = aboutMe.getBoundingClientRect();
+  var aboutMePositionFromTop = aboutMePosition.top + window.pageYOffset;
+  window.scrollTo({
+    top: aboutMePositionFromTop,
+    behavior: "smooth"
+  });
+}
+
+
+
+// function scrollToAboutMe() {
+//   var aboutMe = document.querySelector(".aboutme-title");
+//   var aboutMePosition = aboutMe.getBoundingClientRect();
+//   window.scrollTo({
+//     top: aboutMePosition.top,
+//     behavior: "smooth"
+//   });
+// }
+
+
+
+
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if( target.length ) {
+    event.preventDefault();
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  }
 });
