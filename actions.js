d9e2f537356690efd8811c70b5d8ba6d064e597a
Joey <77253943+jpobzy@users.noreply.github.com>
2023-01-16 22:29:07 -0500

    updates n stuff

--- actions.js
+++ actions.js
@@ -1,7 +1,16 @@
-// $("#scroll-to-top-btn").on("click", function() {
-//   $("html, body").animate({ scrollTop: 0 }, "slow");
-// });
+// select the buttons and images
+const beforeButton = document.querySelector('.before-button');
+const afterButton = document.querySelector('.after-button');
+const beforeImage = document.querySelector('.before-image');
+const afterImage = document.querySelector('.after-image');
 
+// add event listeners to the buttons
+beforeButton.addEventListener('click', () => {
+  beforeImage.style.display = 'block';
+  afterImage.style.display = 'none';
+});
 
-
-
+afterButton.addEventListener('click', () => {
+  beforeImage.style.display = 'none';
+  afterImage.style.display = 'block';
+});
