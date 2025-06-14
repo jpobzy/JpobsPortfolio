dfa9b03a6a36decfe59c9d0b59cdec4cbf754487
Joey <joeypobl@buffalo.edu>
2025-06-14 16:44:41 -0400

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Footer from './components/footer/footer'
-import Aurora from './components/aurora/Aurora'
-import Home from './pages/Home.jsx'
-import DevU from './pages/DevU'
-import { Routes, Route} from 'react-router-dom';
-import ScrollToTop from './components/scrollToTop/ScrollToTop';
-import { BrowserRouter } from 'react-router-dom'
-import FlipArena from './pages/FlipArena';
-import TuneRip from './pages/TuneRip';
-// import Header from './components/header/Header'
+// import Footer from './components/footer/footer'
+// import Aurora from './components/aurora/Aurora'
+// import Home from './pages/Home.jsx'
+// import DevU from './pages/DevU'
+// import { Routes, Route} from 'react-router-dom';
+// import ScrollToTop from './components/scrollToTop/ScrollToTop';
+// import { BrowserRouter } from 'react-router-dom'
+// import FlipArena from './pages/FlipArena';
+// import TuneRip from './pages/TuneRip';
+import { Suspense, lazy } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const Home = lazy(() => import('./pages/Home'))
+const DevU = lazy(() => import('./pages/DevU'))
+const FlipArena = lazy(() => import('./pages/FlipArena'))
+const TuneRip = lazy(() => import('./pages/TuneRip'))
+const Footer = lazy(() => import('./components/footer/footer'))
+const Aurora = lazy(() => import('./components/aurora/Aurora'))
+const ScrollToTop = lazy(() => import('./components/scrollToTop/ScrollToTop'))
 
 ;
 import { useEffect } from 'react';
@@ -24,39 +30,41 @@ function App() {
 
   return (
     <>
-      <div>
-
-      <Aurora
-        colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
-        blend={0.5}
-        amplitude={1.0}
-        speed={0.5}
-        />
-      </div>
-
-
-      <BrowserRouter>
-        <ScrollToTop/> 
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/projects/devu" element={<DevU />} />
-        <Route path="/projects/fliparena" element={<FlipArena />} />
-        <Route path='/projects/tunerip' element={<TuneRip />}/>
-      </Routes>
-    </BrowserRouter>
-
-      <Footer/>
-
-      <div style={{ transform: "scaleY(-1)" }} className="pointer-events-none position: relative;">
+    <Suspense fallback={<div>Loading...</div>}>
+        <div>
         <Aurora
           colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
           blend={0.5}
           amplitude={1.0}
           speed={0.5}
-        />
-    </div>
+          />
+        </div>
+
+
+        <BrowserRouter>
+          <ScrollToTop/> 
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/projects/devu" element={<DevU />} />
+          <Route path="/projects/fliparena" element={<FlipArena />} />
+          <Route path='/projects/tunerip' element={<TuneRip />}/>
+        </Routes>
+      </BrowserRouter>
+
+        <Footer/>
+
+        <div style={{ transform: "scaleY(-1)" }} className="pointer-events-none position: relative;">
+          <Aurora
+            colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+            blend={0.5}
+            amplitude={1.0}
+            speed={0.5}
+          />
+      </div>
+    </Suspense>
     </>
   )
 }
+
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
+   build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('framer-motion')) return 'framer';
+            if (id.includes('react-dom')) return 'react-dom';
+            if (id.includes('react')) return 'react';
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
   base: '/'
 })
